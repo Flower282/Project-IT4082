51d2e626ea6b7b70d8e193c6ae558db122c63084
Lu Thanh Duong <duong.lt235691@sis.hust.edu.vn>
2025-05-02 22:29:28 +0700

    Implement password hashing and verification methods using Argon2id in HashPasswordUtil

--- BlueMoonManagement/src/main/java/io/github/ktpm/bluemoonmanagement/util/HashPasswordUtil.java
+++ BlueMoonManagement/src/main/java/io/github/ktpm/bluemoonmanagement/util/HashPasswordUtil.java
@@ -1,5 +1,17 @@
 package io.github.ktpm.bluemoonmanagement.util;
 
+import com.password4j.Password;
+import com.password4j.Hash;
+
 public class HashPasswordUtil {
-    
+    // Băm mật khẩu bằng Argon2id
+    public static String hashPassword(String password) {
+        Hash hash = Password.hash(password).addRandomSalt().withArgon2();
+        return hash.getResult();
+    }
+
+    // Xác thực mật khẩu với hash đã lưu
+    public static boolean verifyPassword(String password, String hash) {
+        return Password.check(password, hash).withArgon2();
+    }
 }
