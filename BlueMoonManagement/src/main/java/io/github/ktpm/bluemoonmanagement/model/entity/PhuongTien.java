1d8f8c156432ac0dcbbd179c5f7a07712203c0e3
Lu Thanh Duong <duong.lt235691@sis.hust.edu.vn>
2025-05-03 16:06:33 +0700

    Refactor PhuongTienDto and PhuongTienMapper to include NoArgsConstructor and improve DTO mapping

--- BlueMoonManagement/src/main/java/io/github/ktpm/bluemoonmanagement/model/entity/PhuongTien.java
+++ BlueMoonManagement/src/main/java/io/github/ktpm/bluemoonmanagement/model/entity/PhuongTien.java
@@ -3,9 +3,11 @@ package io.github.ktpm.bluemoonmanagement.model.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "phuong_tien")
 @Entity
 public class PhuongTien {
