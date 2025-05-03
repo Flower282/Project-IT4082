5c82b95211c75da803b312ca34cd79f5c2438fab
Lu Thanh Duong <duong.lt235691@sis.hust.edu.vn>
2025-05-03 14:20:45 +0700

    Add GeneratePasswordUtil class for secure password generation

---
+++ BlueMoonManagement/src/main/java/io/github/ktpm/bluemoonmanagement/util/GeneratePasswordUtil.java
@@ -0,0 +1,18 @@
+package io.github.ktpm.bluemoonmanagement.util;
+
+import java.security.SecureRandom;
+
+public class GeneratePasswordUtil {
+    private static final int PASSWORD_LENGTH = 10;
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+
+    public static String generate() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(PASSWORD_LENGTH);
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(index));
+        }
+        return sb.toString();
+    }
+}
