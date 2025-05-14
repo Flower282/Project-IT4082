b6fd78a06f185e566eb33a071d63f84b2033d328
Lu Thanh Duong <duong.lt235691@sis.hust.edu.vn>
2025-05-14 22:01:32 +0700

    Add NoArgsConstructor annotation to entity classes: CanHo, CuDan, KhoanThu, and TaiKhoan for improved instantiation support.

--- BlueMoonManagement/src/main/java/io/github/ktpm/bluemoonmanagement/model/entity/CanHo.java
+++ BlueMoonManagement/src/main/java/io/github/ktpm/bluemoonmanagement/model/entity/CanHo.java
@@ -11,9 +11,10 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "can_ho")
 @Entity
 public class CanHo {
