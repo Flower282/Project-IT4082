69ff6c5e3a3d80113f1813cad87723b90e6b3d34
NguyenNgocSon <ngocson0971@gmail.com>
2025-05-31 10:41:59 +0700

    add tests

--- BlueMoonManagement/src/test/java/io/github/ktpm/bluemoonmanagement/util/EmailQuenMatKhauTest.java
+++ BlueMoonManagement/src/test/java/io/github/ktpm/bluemoonmanagement/util/EmailQuenMatKhauTest.java
@@ -3,21 +3,9 @@ package io.github.ktpm.bluemoonmanagement.util;
 import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.mail.javamail.JavaMailSender;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class OtpQuenMatKhauTest {
-    @Test
-    void testGenerateOtpLengthAndRange() {
-        for (int i = 0; i < 100; i++) {
-            String otp = OtpUtil.generateOtp();
-            assertEquals(6, otp.length());
-            int value = Integer.parseInt(otp);
-            assertTrue(value >= 100000 && value <= 999999);
-        }
-    }
-
+class EmailQuenMatKhauTest {
     @SuppressWarnings("null")
     @Test
     void testSendEmailQuenMatKhauSendsMail() {
@@ -34,10 +22,4 @@ class OtpQuenMatKhauTest {
         EmailUtil.sendEmailQuenMatKhau(email, hoTen, otp, mailSender);
         verify(mailSender, atLeastOnce()).send(any(MimeMessage.class));
     }
-
-    // To avoid future JDK issues with Mockito inline mocking, add the following JVM argument when running tests:
-    // -javaagent:"C:/Users/ASUS/.m2/repository/org/mockito/mockito-core/5.2.0/mockito-core-5.2.0.jar"
-    // (Hoặc thay đường dẫn cho đúng với máy của bạn)
-    // Tham khảo thêm: https://javadoc.io/doc/org.mockito/mockito-core/latest/org/mockito/Mockito.html#0.3
-    // Đây chỉ là cảnh báo, test vẫn chạy bình thường trên JDK hiện tại.
 }
